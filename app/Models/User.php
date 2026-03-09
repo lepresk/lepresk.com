fdecad39a9acd2dfc139a4cf048b3c62cbe564cf
lepresk <lepresk@gmail.com>
2026-03-09 21:25:10 +0100

    fix: update canAccessPanel method to always return true

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,6 @@ final class User extends Authenticatable implements FilamentUser, MustVerifyEmai
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->email === config('contact.admin_email');
+        return true;
     }
 }
